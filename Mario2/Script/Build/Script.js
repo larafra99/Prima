10d27b026f9933c5170e2a907cbaba6f74830365
frankela <lara.sofie.franke@hs-furtwangen.de>
2022-10-18 17:14:46 +0200

    unterricht laufen keypressing

--- Mario2/Script/Build/Script.js
+++ Mario2/Script/Build/Script.js
@@ -1,1 +1,75 @@
-console.warn("%cFor scripting, start your favorite IDE (e.g. VSCode) with typescript installed, load and compile this project", "color: red; font-weight: bold;");
+"use strict";
+var Script;
+(function (Script) {
+    var ƒ = FudgeCore;
+    ƒ.Project.registerScriptNamespace(Script); // Register the namespace to FUDGE for serialization
+    class CustomComponentScript extends ƒ.ComponentScript {
+        // Register the script as component for use in the editor via drag&drop
+        static iSubclass = ƒ.Component.registerSubclass(CustomComponentScript);
+        // Properties may be mutated by users in the editor via the automatically created user interface
+        message = "CustomComponentScript added to ";
+        constructor() {
+            super();
+            // Don't start when running in editor
+            if (ƒ.Project.mode == ƒ.MODE.EDITOR)
+                return;
+            // Listen to this component being added to or removed from a node
+            this.addEventListener("componentAdd" /* ƒ.EVENT.COMPONENT_ADD */, this.hndEvent);
+            this.addEventListener("componentRemove" /* ƒ.EVENT.COMPONENT_REMOVE */, this.hndEvent);
+            this.addEventListener("nodeDeserialized" /* ƒ.EVENT.NODE_DESERIALIZED */, this.hndEvent);
+        }
+        // Activate the functions of this component as response to events
+        hndEvent = (_event) => {
+            switch (_event.type) {
+                case "componentAdd" /* ƒ.EVENT.COMPONENT_ADD */:
+                    ƒ.Debug.log(this.message, this.node);
+                    break;
+                case "componentRemove" /* ƒ.EVENT.COMPONENT_REMOVE */:
+                    this.removeEventListener("componentAdd" /* ƒ.EVENT.COMPONENT_ADD */, this.hndEvent);
+                    this.removeEventListener("componentRemove" /* ƒ.EVENT.COMPONENT_REMOVE */, this.hndEvent);
+                    break;
+                case "nodeDeserialized" /* ƒ.EVENT.NODE_DESERIALIZED */:
+                    // if deserialized the node is now fully reconstructed and access to all its components and children is possible
+                    break;
+            }
+        };
+    }
+    Script.CustomComponentScript = CustomComponentScript;
+})(Script || (Script = {}));
+///<reference path="./../../../Aid/Build/FudgeAid.d.ts"/>
+var Script;
+///<reference path="./../../../Aid/Build/FudgeAid.d.ts"/>
+(function (Script) {
+    var ƒ = FudgeCore;
+    ƒ.Debug.info("Main Program Template running!");
+    let viewport;
+    document.addEventListener("interactiveViewportStarted", start);
+    let marioPos;
+    let marioSpeed = 3.0;
+    function start(_event) {
+        viewport = _event.detail;
+        ƒ.Loop.addEventListener("loopFrame" /* ƒ.EVENT.LOOP_FRAME */, update);
+        ƒ.Loop.start(); // start the game loop to continously draw the viewport, update the audiosystem and drive the physics i/a
+        console.log(viewport);
+        let branch = viewport.getBranch();
+        console.log(branch);
+        marioPos = branch.getChildrenByName("MarioPosition")[0]; // get Sprite by name
+        console.log("Mario", marioPos);
+    }
+    function update(_event) {
+        if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.D, ƒ.KEYBOARD_CODE.ARROW_RIGHT])) {
+            marioPos.mtxLocal.translateX(marioSpeed * ƒ.Loop.timeFrameGame / 1000);
+            //marioPos.getComponent(ƒ.ComponentTransform).mtxLocal.translateX(0.01);
+        }
+        if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.A, ƒ.KEYBOARD_CODE.ARROW_LEFT])) {
+            marioPos.mtxLocal.translateX(-marioSpeed * ƒ.Loop.timeFrameGame / 1000);
+        }
+        ƒ.Loop.timeFrameGame;
+        //console.log( ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.W,ƒ.KEYBOARD_CODE.A, ƒ.KEYBOARD_CODE.S,ƒ.KEYBOARD_CODE.D]));
+        // ƒ.Physics.simulate();  // if physics is included and used
+        viewport.draw();
+        ƒ.AudioManager.default.update();
+        // mtx = Matrix 
+    }
+})(Script || (Script = {}));
+//# sourceMappingURL=Script.js.map
--- Mario2/Script/Source/Main.ts
+++ Mario2/Script/Source/Main.ts
@@ -1,20 +1,46 @@
+
+///<reference path="./../../../Aid/Build/FudgeAid.d.ts"/>
 namespace Script {
+  
   import ƒ = FudgeCore;
+  import ƒAid = FudgeAid;
+
   ƒ.Debug.info("Main Program Template running!");
 
   let viewport: ƒ.Viewport;
   document.addEventListener("interactiveViewportStarted", <EventListener>start);
+  let marioPos:ƒ.Node;
+  let marioSpeed: number = 3.0;
 
   function start(_event: CustomEvent): void {
     viewport = _event.detail;
 
     ƒ.Loop.addEventListener(ƒ.EVENT.LOOP_FRAME, update);
-    // ƒ.Loop.start();  // start the game loop to continously draw the viewport, update the audiosystem and drive the physics i/a
+    ƒ.Loop.start();  // start the game loop to continously draw the viewport, update the audiosystem and drive the physics i/a
+    console.log(viewport);
+    let branch: ƒ.Node = viewport.getBranch();
+    console.log(branch);
+    marioPos= branch.getChildrenByName("MarioPosition")[0]; // get Sprite by name
+    console.log("Mario",marioPos);
   }
 
   function update(_event: Event): void {
+    if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.D, ƒ.KEYBOARD_CODE.ARROW_RIGHT])){
+      marioPos.mtxLocal.translateX(marioSpeed*ƒ.Loop.timeFrameGame/1000 );
+      //marioPos.getComponent(ƒ.ComponentTransform).mtxLocal.translateX(0.01);
+    }
+    if (ƒ.Keyboard.isPressedOne([ ƒ.KEYBOARD_CODE.A,ƒ.KEYBOARD_CODE.ARROW_LEFT])){
+      marioPos.mtxLocal.translateX(-marioSpeed*ƒ.Loop.timeFrameGame/1000 );
+    }
+    ƒ.Loop.timeFrameGame
+    
+    //console.log( ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.W,ƒ.KEYBOARD_CODE.A, ƒ.KEYBOARD_CODE.S,ƒ.KEYBOARD_CODE.D]));
     // ƒ.Physics.simulate();  // if physics is included and used
     viewport.draw();
     ƒ.AudioManager.default.update();
+    // mtx = Matrix 
+    
   }
+
+
 }
