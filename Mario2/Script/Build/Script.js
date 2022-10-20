40927687f64e8aa5313d9cf436307f76dadfdd35
frankela <lara.sofie.franke@hs-furtwangen.de>
2022-10-20 10:23:00 +0200

    Mario sprite animation

--- Mario2/Script/Build/Script.js
+++ Mario2/Script/Build/Script.js
@@ -41,13 +41,17 @@ var Script;
 ///<reference path="./../../../Aid/Build/FudgeAid.d.ts"/>
 (function (Script) {
     var ƒ = FudgeCore;
+    var ƒAid = FudgeAid;
     ƒ.Debug.info("Main Program Template running!");
     let viewport;
     document.addEventListener("interactiveViewportStarted", start);
     let marioPos;
     let marioSpeed = 3.0;
+    let marioSprite;
+    let facing = true; // true = right
     function start(_event) {
         viewport = _event.detail;
+        loadSprite();
         ƒ.Loop.addEventListener("loopFrame" /* ƒ.EVENT.LOOP_FRAME */, update);
         ƒ.Loop.start(); // start the game loop to continously draw the viewport, update the audiosystem and drive the physics i/a
         console.log(viewport);
@@ -56,20 +60,43 @@ var Script;
         marioPos = branch.getChildrenByName("MarioPosition")[0]; // get Sprite by name
         console.log("Mario", marioPos);
     }
+    async function loadSprite() {
+        let spriteSheet = new ƒ.TextureImage();
+        await spriteSheet.load("./Images/mariowalkx16.gif");
+        let coat = new ƒ.CoatTextured(undefined, spriteSheet);
+        let animation = new ƒAid.SpriteSheetAnimation("walk", coat);
+        animation.generateByGrid(ƒ.Rectangle.GET(0, 0, 15, 16), 3, 12, ƒ.ORIGIN2D.BOTTOMCENTER, ƒ.Vector2.X(16));
+        marioSprite = new ƒAid.NodeSprite("Sprite");
+        marioSprite.addComponent(new ƒ.ComponentTransform(new ƒ.Matrix4x4()));
+        marioSprite.setAnimation(animation);
+        marioSprite.setFrameDirection(1);
+        marioSprite.mtxLocal.translateY(+0.5); // mtx = Matrix 
+        marioSprite.framerate = 10;
+        marioPos.removeAllChildren();
+        marioPos.addChild(marioSprite);
+        viewport.draw();
+    }
     function update(_event) {
         if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.D, ƒ.KEYBOARD_CODE.ARROW_RIGHT])) {
             marioPos.mtxLocal.translateX(marioSpeed * ƒ.Loop.timeFrameGame / 1000);
+            if (!facing) {
+                marioSprite.getComponent(ƒ.ComponentTransform).mtxLocal.rotateY(180);
+                facing = true;
+            }
             //marioPos.getComponent(ƒ.ComponentTransform).mtxLocal.translateX(0.01);
         }
         if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.A, ƒ.KEYBOARD_CODE.ARROW_LEFT])) {
             marioPos.mtxLocal.translateX(-marioSpeed * ƒ.Loop.timeFrameGame / 1000);
+            if (facing) {
+                marioSprite.getComponent(ƒ.ComponentTransform).mtxLocal.rotateY(180);
+                facing = false;
+            }
         }
         ƒ.Loop.timeFrameGame;
         //console.log( ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.W,ƒ.KEYBOARD_CODE.A, ƒ.KEYBOARD_CODE.S,ƒ.KEYBOARD_CODE.D]));
         // ƒ.Physics.simulate();  // if physics is included and used
         viewport.draw();
         ƒ.AudioManager.default.update();
-        // mtx = Matrix 
     }
 })(Script || (Script = {}));
 //# sourceMappingURL=Script.js.map
--- Mario2/Script/Source/Main.ts
+++ Mario2/Script/Source/Main.ts
@@ -11,9 +11,12 @@ namespace Script {
   document.addEventListener("interactiveViewportStarted", <EventListener>start);
   let marioPos:ƒ.Node;
   let marioSpeed: number = 3.0;
+  let marioSprite: ƒAid.NodeSprite;
+  let facing: boolean = true; // true = right
 
   function start(_event: CustomEvent): void {
     viewport = _event.detail;
+    loadSprite();
 
     ƒ.Loop.addEventListener(ƒ.EVENT.LOOP_FRAME, update);
     ƒ.Loop.start();  // start the game loop to continously draw the viewport, update the audiosystem and drive the physics i/a
@@ -24,13 +27,41 @@ namespace Script {
     console.log("Mario",marioPos);
   }
 
+  async function loadSprite():Promise<void>{
+    let spriteSheet: ƒ.TextureImage = new ƒ.TextureImage();
+    await spriteSheet.load("./Images/mariowalkx16.gif");
+    let coat: ƒ.CoatTextured = new ƒ.CoatTextured(undefined, spriteSheet);
+
+    let animation: ƒAid.SpriteSheetAnimation = new ƒAid.SpriteSheetAnimation("walk", coat);
+    animation.generateByGrid(ƒ.Rectangle.GET(0, 0, 15, 16), 3, 12, ƒ.ORIGIN2D.BOTTOMCENTER, ƒ.Vector2.X(16));
+    marioSprite = new ƒAid.NodeSprite("Sprite");
+    marioSprite.addComponent(new ƒ.ComponentTransform(new ƒ.Matrix4x4()));
+    marioSprite.setAnimation(animation);
+    marioSprite.setFrameDirection(1);
+    marioSprite.mtxLocal.translateY(+0.5); // mtx = Matrix 
+    marioSprite.framerate = 10;
+    marioPos.removeAllChildren();
+    marioPos.addChild(marioSprite);
+    viewport.draw();
+  }
+
   function update(_event: Event): void {
+    
     if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.D, ƒ.KEYBOARD_CODE.ARROW_RIGHT])){
       marioPos.mtxLocal.translateX(marioSpeed*ƒ.Loop.timeFrameGame/1000 );
+      if (!facing){
+        marioSprite.getComponent(ƒ.ComponentTransform).mtxLocal.rotateY(180)
+        facing = true;
+      }
       //marioPos.getComponent(ƒ.ComponentTransform).mtxLocal.translateX(0.01);
     }
     if (ƒ.Keyboard.isPressedOne([ ƒ.KEYBOARD_CODE.A,ƒ.KEYBOARD_CODE.ARROW_LEFT])){
       marioPos.mtxLocal.translateX(-marioSpeed*ƒ.Loop.timeFrameGame/1000 );
+      
+      if (facing){
+        marioSprite.getComponent(ƒ.ComponentTransform).mtxLocal.rotateY(180)
+        facing = false;
+      }
     }
     ƒ.Loop.timeFrameGame
     
@@ -38,7 +69,7 @@ namespace Script {
     // ƒ.Physics.simulate();  // if physics is included and used
     viewport.draw();
     ƒ.AudioManager.default.update();
-    // mtx = Matrix 
+    
     
   }
 
