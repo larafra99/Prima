41bb5ae485ab9dd37576b6228803308397141a08
frankela <lara.sofie.franke@hs-furtwangen.de>
2023-02-06 10:11:47 +0100

    camera angle, vui, check on Field

--- HarvestFarm/Script/Source/CharacterEventScript.ts
+++ HarvestFarm/Script/Source/CharacterEventScript.ts
@@ -8,8 +8,6 @@ namespace Harvest {
       // Properties may be mutated by users in the editor via the automatically created user interface
       
       public eventAudio: ƒ.ComponentAudio;
-      private stamina: number;
-      private vitality: number;
       private startpoint:ƒ.Matrix4x4;
   
   
@@ -19,7 +17,7 @@ namespace Harvest {
         // Don't start when running in editor
         if (ƒ.Project.mode == ƒ.MODE.EDITOR)
           return;
-  
+
         // Listen to this component being added to or removed from a node
         this.addEventListener(ƒ.EVENT.COMPONENT_ADD, this.hndEvent);
         this.addEventListener(ƒ.EVENT.COMPONENT_REMOVE, this.hndEvent);
@@ -36,21 +34,36 @@ namespace Harvest {
             this.removeEventListener(ƒ.EVENT.COMPONENT_REMOVE, this.hndEvent);
             break;
           case ƒ.EVENT.NODE_DESERIALIZED:
-            console.log("Startpoint", this.startpoint);
+            //this.startNewDay();
+            this.node.addEventListener(ƒ.EVENT.RENDER_PREPARE, this.update);
             // if deserialized the node is now fully reconstructed and access to all its components and children is possible
             break;
         }
       }
+
+      private update = (_event: Event): void => {
+        this.getDistance();
+        if (!playerstate){
+          return;
+        }
+    
+      }
+
       private startNewDay():void{
         this.node.mtxLocal.set(this.startpoint);
-        this.stamina= playerstate.stamina;
-        this.vitality= playerstate.vitality;
-
+        playerstate.stamina= stamina;
+        playerstate.vitality = vitality;
+        playerstate.day++;
       }
-  
-      // protected reduceMutator(_mutator: ƒ.Mutator): void {
-      //   // delete properties that should not be mutated
-      //   // undefined properties and private fields (#) will not be included by default
-      // }
+      
+      private getDistance(){
+        if(this.node.mtxWorld.translation.x>-5 && this.node.mtxWorld.translation.x<5 && this.node.mtxWorld.translation.z>-5 && this.node.mtxWorld.translation.z<5){
+          //console.log("Yes");
+          onField= true;
+        }
+        else{
+          onField = false;
+        }
+    }
     }
   }
